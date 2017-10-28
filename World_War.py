0113c2bb07dfcd041ce34be48811d31a3918e523
Ernie <ernieyeung123@gmail.com>
2017-10-27 20:25:38 -0700

    intial

---
+++ World_War.py
@@ -0,0 +1,45 @@
+
+
+#<METADATA>
+QUIET_VERSION = "0.1"
+PROBLEM_NAME = "Basic Eight Puzzle"
+PROBLEM_VERSION = "0.0"
+PROBLEM_AUTHORS = ['E.Yeung','']
+PROBLEM_CREATION_DATE = "19-OCT-2017"
+PROBLEM_DESC=""
+#</METADATA>
+
+#<COMMON_CODE>
+class State:
+    def __init__(self, d):
+        self.d = d
+
+    def __str__(self):
+        return ""
+
+    def can_move(self,spaces):
+        index = 0
+    
+    def move(self,spaces):
+        return ""
+
+
+
+class Country:
+    def __init__(self, d):
+        self.d = d
+        self.data = {}
+        
+#</COMMON_CODE>
+
+#<INITIAL_STATE>
+
+#</INITIAL_STATE>
+
+
+#<OPERATORS>
+
+        
+#</OPERATORS>
+
+
