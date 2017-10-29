096dfbd3bcfa153a2d00daf2b91f9ebc4c98f83e
sarthakturkhia <sarthakturakhia@gmail.com>
2017-10-28 18:04:05 -0700

    added country class

--- World_War.py
+++ World_War.py
@@ -26,9 +26,55 @@ class State:
 
 
 class Country:
-    def __init__(self, d):
-        self.d = d
+    def __init__(self, name, army, gdp, nato, nuclear, w_i, un):
+        self.name = name
         self.data = {}
+        self.army = army
+        self.gdp = gdp
+        self.nato = nato
+        self.nuclear = nuclear
+        self.w_i = w_i
+        self.un = un
+
+    def describe(self):
+        result = "\n"
+        result += self.name + " has army size of about" + str(self.army*100000) + " with a gdp of " + str(self.gdp*100000) + ". "
+        if(self.is_nato()):
+            result += self.name + "is also part of nato. "
+        if(self.is_in_un()):
+            result += "This country is a permanent member of the UN. "
+        if(self.is_nuclear()):
+            result += "And it has Nuclear Weapons. "
+        result += self.name + " has a ranking of " + str(self.world_influence()) + "in world influence."
+        return result
+
+    def is_nuclear(self):
+        return self.nuclear
+
+    def troop_size(self):
+        return self.army
+
+    def gdp(self):
+        return self.gdp
+
+    def is_nato(self):
+        return self.nato
+
+    def update_gdp(self):
+        self.gdp += self.gdp * 0.00083
+
+    def update_world_influence(self, w):
+        self.w_i = w
+
+    def world_influence_index(self):
+        return self.army/20 + self.gdp + self.nuclear*10 + self.un*10
+
+    def world_influence(self):
+        return self.w_i
+
+    def is_in_un(self):
+        return self.un
+
         
 #</COMMON_CODE>
 
